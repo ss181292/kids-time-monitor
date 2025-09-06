2ddcdc14cb5229d798107b2ca13b5bf04569baf2
Staszek S. <ss181292@wp.pl>
2025-09-06 14:15:55 +0200

    Initial commit

---
+++ src/main/java/it/skowron/kids_time_monitor/KidsTimeMonitorApplication.java
@@ -0,0 +1,13 @@
+package it.skowron.kids_time_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsTimeMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsTimeMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/skowron/kids_time_monitor/KidsTimeMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package it.skowron.kids_time_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsTimeMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
